89bc25044f260506ce3451115ac635de4e1ff4cf
Douglas <douglasdoda@hotmail.com>
2025-04-15 08:12:17 -0300

    Estudos
    Estudos

--- Todas Aulas/Aula 13/ambiente.js
+++ Todas Aulas/Aula 13/ambiente.js
@@ -14,7 +14,7 @@
 
 
 console.log('Vai Começar')
-    for( var c=1 ; c<=6 ; c++){
+    for( var c=1 ; c<=3 ; c++){
         console.log(c)
     }
     console.log('Fim')
---
+++ Todas Aulas/Aula 14/script.js
@@ -0,0 +1,5 @@
+function contar() {
+    let ini = document.querySelector('txti')
+    let fim = document.querySelector('txtf')
+    let passo = document.querySelector('txtp')
+}
