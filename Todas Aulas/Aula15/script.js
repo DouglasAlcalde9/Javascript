7d3db9ef64bf1301fa7e2a4d16aee1f29fdcb9f8
Douglas <douglasdoda@hotmail.com>
2025-05-25 16:13:50 -0300

    Att
    Att

--- Todas Aulas/Aula15/script.js
+++ Todas Aulas/Aula15/script.js
@@ -1,7 +1,15 @@
-let num = [5,8,2,9,3]
+let num = [5, 8, 2, 9, 3]
 num.push(1)
 num.sort()
-console.log (` O vetor tem ${num.length} posições `)
+console.log(` O vetor tem ${num.length} posições `)
 console.log(num)
 console.log(`A primeira posição é ${num[0]}`)
 
+let pos = num.indexOf(5)
+
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`A posição  esta na posição ${pos}`)
+}
+ 
