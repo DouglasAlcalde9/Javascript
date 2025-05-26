2b291b58466955db217eb1424e2274d33c57ac04
Douglas <douglasdoda@hotmail.com>
2025-05-26 11:08:11 -0300

    Att
    Att

---
+++ Todas Aulas/Aula16/funcao01.js
@@ -0,0 +1,17 @@
+function parimpar(n) {
+    if(n % 2 == 0 ) {
+        return  'Par!'
+    } else{
+        return 'Impar!'
+    }
+        
+}
+
+  /*
+  
+  let res = parimpar(10)
+ console.log(res)  
+ 
+ tambem posso usar */
+
+ console.log(parimpar(3))
